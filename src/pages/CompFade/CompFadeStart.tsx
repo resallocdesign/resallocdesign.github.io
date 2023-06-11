9483204d1f52af225cebff673cedd9d4749a015d
Arnav Verma <arnav.verma@mail.utoronto.ca>
2023-06-11 18:57:54 -0400

    added author information and paper link

--- src/pages/CompFade/CompFadeStart.tsx
+++ src/pages/CompFade/CompFadeStart.tsx
@@ -2,19 +2,19 @@ import { useState } from "react";
 import { useHistory } from "react-router-dom";
 
 const exp1_2_cond = [
-{ name: "(text+vis, individual)", uri: "vp" },
-{ name: "(text+vis, group)", "uri": "vc" },
-{ name: "(text, individual)", "uri": "np" },
-{ name: "(text, group)", "uri": "nc" }]
+  { name: "(text+vis, individual)", uri: "vp" },
+  { name: "(text+vis, group)", "uri": "vc" },
+  { name: "(text, individual)", "uri": "np" },
+  { name: "(text, group)", "uri": "nc" }]
 const exp3_cond = [
-  { name: "(textNone, visGroup)", uri: "nc"}, 
-  { name: "(textIndividual, visGroup)", uri: "pc"}, 
-  { name: "(textGroup, visNone)", uri: "cn"}, 
-  { name: "(textGroup, visGroup)", uri: "cc"}, 
-  { name: "(textGroup, visIndividual)", uri: "cp"}, 
-  { name: "(textIndividual, visNone)", uri: "pn"}, 
-  { name: "(textIndividual, visGroup)", uri: "pc"}, 
-  { name: "(textIndividual, visIndividual)", uri: "pp"}]
+  { name: "(textNone, visGroup)", uri: "nc" },
+  { name: "(textIndividual, visGroup)", uri: "pc" },
+  { name: "(textGroup, visNone)", uri: "cn" },
+  { name: "(textGroup, visGroup)", uri: "cc" },
+  { name: "(textGroup, visIndividual)", uri: "cp" },
+  { name: "(textIndividual, visNone)", uri: "pn" },
+  { name: "(textIndividual, visGroup)", uri: "pc" },
+  { name: "(textIndividual, visIndividual)", uri: "pp" }]
 
 function CompFadeStart({ nextState }: { nextState: Function }) {
 
@@ -47,10 +47,18 @@ function CompFadeStart({ nextState }: { nextState: Function }) {
         <h2 className="mt-4 mb-2 text-2xl font-bold text-center text-gray-800">
           Designing Resource Allocation Tools to Promote Fair Allocation
         </h2>
-        <h3 className="mb-8 text-xl text-center text-gray-800">
+        <h3 className="mb-2 text-xl text-center text-gray-800">
           Does Visualization and Information Framing Matter?
         </h3>
-        <div className="mb-2 ml-8 text-xl ">
+        <h4 className="text-center">
+          <a className="text-blue-600 underline hover:text-blue-400" target="_blank" rel="noreferrer" href="https://www.arnavverma.com/">Arnav Verma</a>,{" "}
+          <a className="text-blue-600 underline hover:text-blue-400" target="_blank" rel="noreferrer" href="https://luizaugustomm.github.io/">Luiz Morais</a>, {" "}
+          <a className="text-blue-600 underline hover:text-blue-400" target="_blank" rel="noreferrer" href="http://dragice.fr/">Pierre Dragicevic</a>, {" "}
+          <a className="text-blue-600 underline hover:text-blue-400" target="_blank" rel="noreferrer" href="http://fannychevalier.net/">Fanny Chevalier</a>
+        </h4>
+        
+        <h4 className="ml-4">Paper Link:  <a target="_blank" rel="noreferrer" href="https://doi.org/10.1145/3544548.3580739" className="text-blue-600 underline hover:text-blue-400">https://doi.org/10.1145/3544548.3580739</a></h4>
+        <div className="mb-2 ml-4 ">
           Click on one of the conditions below to start an experiment!
         </div>
         <div className="flex flex-col w-full lg:flex-row ">
@@ -98,17 +106,17 @@ const ExpBlock: React.FC<{ exp: IExp; onClick: () => void; isSelected: boolean }
       <h3 className="text-lg font-bold"> Conditions:</h3>
       <div className="flex flex-wrap ml-6 space-x-4">
         {exp.title === "Experiment 3" && exp3_cond.map((c) => {
-          return <button  className="px-2 py-1 mt-2 bg-gray-300 rounded-lg cursor-pointer hover:bg-yellow-400" 
-          onClick={() => { history.push("/exp3?cond=" + c.uri); }}> {c.name} </button>
+          return <button className="px-2 py-1 mt-2 bg-gray-300 rounded-lg cursor-pointer hover:bg-yellow-400"
+            onClick={() => { history.push("/exp3?cond=" + c.uri); }}> {c.name} </button>
         })}
         {exp.title !== "Experiment 3" && exp1_2_cond.map((c) => {
-          return <button  className="px-2 py-1 mt-2 bg-gray-300 rounded-lg cursor-pointer hover:bg-yellow-400" 
-          onClick={() => { 
-            if (exp.title === "Experiment 1") history.push("/exp1?cond=" + c.uri);
-            if (exp.title === "Experiment 2") history.push("/exp2?cond=" + c.uri);
-          }}> 
-            {c.name} 
-            
+          return <button className="px-2 py-1 mt-2 bg-gray-300 rounded-lg cursor-pointer hover:bg-yellow-400"
+            onClick={() => {
+              if (exp.title === "Experiment 1") history.push("/exp1?cond=" + c.uri);
+              if (exp.title === "Experiment 2") history.push("/exp2?cond=" + c.uri);
+            }}>
+            {c.name}
+
           </button>
         })}
       </div>
